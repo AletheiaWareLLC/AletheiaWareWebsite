f7bb3876c93356e03ab4025afcd9fb8c503bb404
Stuart Scott <stuart.murray.scott@gmail.com>
2020-12-30 15:32:05 -0800

    Drop .html extension from vanity URLs

--- generate-vanity.sh
+++ generate-vanity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-while read p; do sed -e "s/\${project}/${p}/g" aw-vanity-template.html > html/static/${p}.html; done < packages
+while read p; do sed -e "s/\${project}/${p}/g" aw-vanity-template.txt > html/static/${p}; done < packages
 
