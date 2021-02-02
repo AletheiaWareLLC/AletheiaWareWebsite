ed8e22b75cbec9101fb5fb10d7941ca5782b2982
Stuart Scott <stuart.murray.scott@gmail.com>
2021-02-02 09:25:22 -0800

    Move Vanity URLs into directories

--- generate-vanity.sh
+++ generate-vanity.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-while read p; do sed -e "s/\${project}/${p}/g" aw-vanity-template.txt > html/static/${p}; done < packages
+while read p; do mkdir -p html/static/${p}/ && sed -e "s~\${project}~${p}~g" aw-vanity-template.txt > html/static/${p}/index.html; done < packages
 
