a593cdd9034a6155e9376c86f8af0da3a831c1dc
Stuart Scott <stuart.murray.scott@gmail.com>
2021-02-02 09:39:33 -0800

    Vanity URLs to subdirectories point to base project

--- generate-vanity.sh
+++ generate-vanity.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-while read p; do mkdir -p html/static/${p}/ && sed -e "s~\${project}~${p}~g" aw-vanity-template.txt > html/static/${p}/index.html; done < packages
+while read p
+do
+	mkdir -p html/static/${p}/
+	base="$(cut -d'/' -f1 <<<${p})"
+	sed -e "s/\${base}/${base}/g" -e "s~\${full}~${p}~g" aw-vanity-template.txt > html/static/${p}/index.html
+done < packages
 
